420bc315e237636974546750c52612192cea7992
OtbeyNat <72035811+OtbeyNat@users.noreply.github.com>
2024-06-21 20:30:21 -0400

    portfolio

---
+++ data/index.ts
@@ -0,0 +1,227 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Experience", link: "#experience" },
+    { name: "Projects", link: "#projects" },
+    { name: "Gallery", link: "#gallery" },
+    { name: "Contact", link: "#contact" },
+    { name: "↑", link: "" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Prioritizing Extensive Preparation and Constant Communication",
+      description: "I am best known ",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "Open to Travel to My Next Destination",
+      description: "For my future I am",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "Include Important and Modern Technologies",
+      description: "My primary skills",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech Enthusiast with a Passion for Development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Building New Applications and Expanding My Repertoire",
+      description: "I am consistently",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Please View My Resume Below",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Evently - Event Organization Platform",
+      des: "A platform to explore, host, and join events featuring user authentication and payment systems using the latest tech stack.",
+      img: "/gallery/evently-cover.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "mongodb.svg", "/c.svg", "/stripe.svg"],
+      link: "https://event-platform-steel-nine.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "Yoom - Video Conferencing App",
+      des: "NextJS Zoom Clone Application. Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+      img: "/gallery/yoom-cover.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://zoom-clone-sooty-beta.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Threads Clone App",
+      des: "Social Media Clone Application of Threads featuring the ability to post threads, reply to messages, and follow friends. Built using NextJS, Typescript, MongoDB and more.",
+      img: "/gallery/threads-cover.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "mongodb.svg", "/c.svg"],
+      link: "https://github.com/OtbeyNat/threads_app",
+    },
+    {
+      id: 4,
+      title: "Python Games Library",
+      des: "Created modified versions of classic two-dimensional games such as snake, flappybird, street fighter and other platformers with Python library Pygame",
+      img: "/gallery/fight_screenshot.png",
+      iconLists: ["python.svg", "pygame.svg"],
+      link: "https://github.com/OtbeyNat/pythonapps",
+    },
+  ];
+  
+  export const gallery = [
+    {
+      title: "Evently - Home Page",
+      img: "/gallery/evently-cover.png",
+    },
+    {
+      title: "Evently - View Tickets",
+      img: "/gallery/evently-1.png",
+    },
+    {
+      title: "Evently - Event Details",
+      img: "/gallery/evently-4.png",
+    },
+    {
+      title: "Yoom - Home Page",
+      img: "/gallery/yoom-cover.png",
+    },
+    {
+      title: "Yoom - Video Call",
+      img: "/gallery/yoom-1.png",
+    },
+    {
+      title: "2D Fighter - Action Screenshot",
+      img: "/gallery/fight_screenshot.png",
+    },
+    {
+      title: "Flappybird - Start",
+      img: "/gallery/flappy_screenshot_2.png",
+    },
+    {
+      title: "Flappybird - Game Over",
+      img: "/gallery/flappy_screenshot_1.png",
+    },
+    {
+      title: "Platform Shooter - Action Screenshot",
+      img: "/gallery/shooter_screenshot.png",
+    },    
+    {
+      title: "J&J - Whatfix Data Visualization Screenshot",
+      img: "/gallery/whatfix.png",
+    },    
+  ];
+    
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Whatfix Data Analytics Team Lead - Johnson & Johnson",
+      desc: "Oversaw a team of RPI graduate students and enhanced the value of J&J's adoption of Whatfix by developing a React application and over 20 data visualizations in Jupyter Notebook.",
+      className: "md:col-span-2",
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 2,
+      title: "Freelance Web Applcation Developer - Agoro",
+      desc: "Developed a discussion forum to facilitate communication between 100+ students and their professors, creating training data for their artificial intelligent teaching assistant using React and Go",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 3,
+      title: "Software Developer Intern - MoleQ, Inc.",
+      desc: "Implemented an interactive, customizable user interface used to display over 100 associated products and functions on Point-of-Sale (POS) Android software.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+    {
+      id: 4,
+      title: "Computer Science Mentor - Rensselaer Polytechnic Institute",
+      desc: "Supported RPI students in their learning of Introduction to Computer Science and Data Structures by analyzing code and teaching fundamental programming concepts",
+      className: "md:col-span-2",
+      thumbnail: "/exp3.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/OtbeyNat?tab=repositories"
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/tobey-tan3",
+    },
+  ];
+
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
