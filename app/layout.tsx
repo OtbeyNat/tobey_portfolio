db07ae2dadec8eb3d9de5ade36fbf1e224f03df5
OtbeyNat <72035811+OtbeyNat@users.noreply.github.com>
2024-06-21 20:53:19 -0400

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Tobey Tan",
   description: "Modern & Minimalist Web Portfolio for Tobey Tan",
+  icons: {
+    icon: '/yoshi.png'
+  }
 };
 
 export default function RootLayout({
