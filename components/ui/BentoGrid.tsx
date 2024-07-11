e1002988c4e9926b2eeabeaaac1b1112834708d5
OtbeyNat <72035811+OtbeyNat@users.noreply.github.com>
2024-07-11 14:13:26 -0400

    resume-update-7-11

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -174,7 +174,7 @@ export const BentoGridItem = ({
               {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
               {/* add handleCopy() for the copy the text */}
               <a
-              href="https://drive.google.com/file/d/1y61DKWFEq5nRlHAwZjZCyb8qKMOUI5mI/view"
+              href="https://drive.google.com/file/d/1RXggak_v1txHX0PgzR8xCP1xTdSyjNPS/view?usp=sharing"
               target={"_blank"}
               >
                 <MagicButton
