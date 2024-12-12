d4f34ca00cfdccbe8406d8794d32453750b23de3
Tobey Tan <tctan317@gmail.com>
2024-12-11 20:38:05 -0500

    spring and ruby

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -174,7 +174,7 @@ export const BentoGridItem = ({
               {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
               {/* add handleCopy() for the copy the text */}
               <a
-              href="https://drive.google.com/file/d/19VtJsIIUM6UiifH2J7kWv64tCOFyusuL/view?usp=sharing"
+              href="https://drive.google.com/file/d/1MVlnVQJzfULu1wbaEULq5k3g9kgVeucr/view?usp=sharing"
               target={"_blank"}
               >
                 <MagicButton
