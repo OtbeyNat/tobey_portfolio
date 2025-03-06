9ac11a0358b898d67452ef030ba9cd649db950aa
Tobey Tan <tctan317@gmail.com>
2025-03-05 20:59:59 -0500

    spotify lite project date update

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -174,7 +174,7 @@ export const BentoGridItem = ({
               {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
               {/* add handleCopy() for the copy the text */}
               <a
-              href="https://drive.google.com/file/d/1qwHHLvySku_a0vNkI4Q3Q6aP0kq1kdGC/view?usp=sharing"
+              href="https://drive.google.com/file/d/1YOflxA3TJ-8TN6abwds128GQ4mQob-Jf/view?usp=sharing"
               target={"_blank"}
               >
                 <MagicButton
